285b695a518829551c0373187ea1188ab03ff4b5
Philip Kuryloski <kuryloskip@vmware.com>
2020-05-29 12:05:30 +0200

    Encode the full honeycomb json with jsx

--- test/honeycomb_cth.erl
+++ test/honeycomb_cth.erl
@@ -9,8 +9,8 @@
 -record(state, {directory, github_workflow, github_run_id,
                 github_repository, github_sha, github_ref,
                 base_rmq_ref, erlang_version, elixir_version,
-                otp_release, cpu_topology_json, schedulers,
-                system_architecture, system_memory_data_json,
+                otp_release, cpu_topology, schedulers,
+                system_architecture, system_memory_data,
                 start_times = #{}}).
 
 id(Opts) ->
@@ -28,10 +28,10 @@ init(Id, _Opts) ->
                 erlang_version = os:getenv("ERLANG_VERSION", "unknown"),
                 elixir_version = os:getenv("ELIXIR_VERSION", "unknown"),
                 otp_release = erlang:system_info(otp_release),
-                cpu_topology_json = cpu_topology_json(erlang:system_info(cpu_topology)),
+                cpu_topology = erlang:system_info(cpu_topology),
                 schedulers = erlang:system_info(schedulers),
                 system_architecture = erlang:system_info(system_architecture),
-                system_memory_data_json = json_string_memory(memsup:get_system_memory_data())}}.
+                system_memory_data = memsup:get_system_memory_data()}}.
 
 pre_init_per_testcase(_Suite, TC, Config, #state{start_times = StartTimes} = State) ->
     {Config, State#state{start_times = StartTimes#{TC => erlang:timestamp()}}}.
@@ -45,10 +45,10 @@ post_end_per_testcase(Suite, TC, _Config, Return, #state{github_workflow = Githu
                                                          erlang_version = ErlangVersion,
                                                          elixir_version = ElixirVersion,
                                                          otp_release = OtpRelease,
-                                                         cpu_topology_json = CpuTopologyJson,
+                                                         cpu_topology = CpuTopology,
                                                          schedulers = Schedulers,
                                                          system_architecture = SystemArchitecture,
-                                                         system_memory_data_json = SystemMemoryDataJson,
+                                                         system_memory_data = SystemMemoryData,
                                                          start_times = StartTimes} = State) ->
     EndTime = erlang:timestamp(),
     {StartTime, StartTimes1} = maps:take(TC, StartTimes),
@@ -56,33 +56,27 @@ post_end_per_testcase(Suite, TC, _Config, Return, #state{github_workflow = Githu
     File = filename(Suite, TC, State),
     ok = filelib:ensure_dir(File),
     {ok, F} = file:open(File, [write]),
-    io:format(F, "{"
-              "\"ci\":\"GitHub Actions\","
-              "\"github_workflow\":\"~s\","
-              "\"github_run_id\":\"~s\","
-              "\"github_repository\":\"~s\","
-              "\"github_sha\":\"~s\","
-              "\"github_ref\":\"~s\","
-              "\"base_rmq_ref\":\"~s\","
-              "\"erlang_version\":\"~s\","
-              "\"elixir_version\":\"~s\","
-              "\"otp_release\":\"~s\","
-              "\"cpu_topology\":~s,"
-              "\"schedulers\":~p,"
-              "\"system_architecture\":\"~s\","
-              "\"system_memory_data\":~s,"
-              "\"suite\":\"~p\","
-              "\"testcase\":\"~p\","
-              "\"duration_seconds\":~p,"
-              "\"result\":~s"
-              "}~n", [GithubWorkflow, GithubRunId, GithubRepository,
-                      GithubSha, GithubRef, BaseRmqRef, ErlangVersion,
-                      ElixirVersion, OtpRelease,
-                      CpuTopologyJson, Schedulers,
-                      SystemArchitecture, SystemMemoryDataJson,
-                      Suite, TC,
-                      DurationMicroseconds / 1000000,
-                      jsx:encode(list_to_binary(io_lib:format("~p", [Return])))]),
+
+    Json = jsx:encode([{<<"ci">>, <<"GitHub Actions">>},
+                       {<<"github_workflow">>, list_to_binary(GithubWorkflow)},
+                       {<<"github_run_id">>, list_to_binary(GithubRunId)},
+                       {<<"github_repository">>, list_to_binary(GithubRepository)},
+                       {<<"github_sha">>, list_to_binary(GithubSha)},
+                       {<<"github_ref">>, list_to_binary(GithubRef)},
+                       {<<"base_rmq_ref">>, list_to_binary(BaseRmqRef)},
+                       {<<"erlang_version">>, list_to_binary(ErlangVersion)},
+                       {<<"elixir_version">>, list_to_binary(ElixirVersion)},
+                       {<<"otp_release">>, list_to_binary(OtpRelease)},
+                       {<<"cpu_topology">>, cpu_topology_json_term(CpuTopology)},
+                       {<<"schedulers">>, Schedulers},
+                       {<<"system_architecture">>, list_to_binary(SystemArchitecture)},
+                       {<<"system_memory_data">>, memory_json_term(SystemMemoryData)},
+                       {<<"suite">>, list_to_binary(atom_to_list(Suite))},
+                       {<<"testcase">>, list_to_binary(atom_to_list(TC))},
+                       {<<"duration_seconds">>, DurationMicroseconds / 1000000},
+                       {<<"result">>, list_to_binary(io_lib:format("~p", [Return]))}]),
+
+    file:write(F, Json),
     file:close(F),
     {Return, State#state{start_times = StartTimes1}}.
 
@@ -90,12 +84,10 @@ filename(Suite, TC, #state{directory = Dir}) ->
     filename:join(Dir, atom_to_list(Suite) ++ "_" ++ atom_to_list(TC)
                   ++ ".json").
 
-json_string_memory(SystemMemoryData) when is_list(SystemMemoryData) ->
-    "{" ++ string:join([io_lib:format("\"~p\":~p", [K, V])
-                        || {K, V} <- SystemMemoryData], ",") ++ "}".
+memory_json_term(SystemMemoryData) when is_list(SystemMemoryData) ->
+    [{list_to_binary(atom_to_list(K)), V} || {K, V} <- SystemMemoryData].
 
-cpu_topology_json([{processor, Cores}]) when is_list(Cores) ->
-    "{\"processor\":["
-        ++ string:join([io_lib:format("{\"core\":{\"~p\":~p}}", [Kind, Index])
-                        || {core, {Kind, Index}} <- Cores], ",")
-        ++ "]}".
+cpu_topology_json_term([{processor, Cores}]) when is_list(Cores) ->
+    [{<<"processor">>, [begin
+                            [{<<"core">>, [{list_to_binary(atom_to_list(Kind)), Index}]}]
+                        end || {core, {Kind, Index}} <- Cores]}].
